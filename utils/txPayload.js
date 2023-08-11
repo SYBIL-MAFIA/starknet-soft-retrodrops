6cf2be2e6b9dc180b1e328ba3f934fcbef1c72e4
fraggdiller <fraggdiller@gmail.com>
2023-08-11 14:39:49 +0300

    Fixed waiting for transaction and updating nonce

--- utils/txPayload.js
+++ utils/txPayload.js
@@ -1,7 +1,8 @@
-import { explorerTx } from "./other.js";
+import { explorerTx } from './other.js';
 
-export default class txСonfirmation {
-    constructor (txPayload,account,provider,logger,moduleString){
+
+export default class txConfirmation {
+    constructor (txPayload, account, provider, logger, moduleString) {
         this.txPayload = txPayload
         this.account = account
         this.provider = provider
@@ -9,41 +10,48 @@ export default class txСonfirmation {
         this.moduleString = moduleString
     }
 
-    async execute(){
+    async execute() {
 
         try {
-            
-            const nonceCash = await this.account.getNonce()
+            let nonceCash
+            try {
+                nonceCash = await this.account.getNonce()
+            } catch (e) {
+                nonceCash = 0
+            }
+
             const executeHash = await this.account.execute(this.txPayload);
             this.logger.info(`${this.moduleString} - Send TX: ${explorerTx.Starknet + executeHash.transaction_hash}`);
             this.logger.info(`${this.moduleString} - Waiting for tx status... `)
-            const res = await this.provider.waitForTransaction(executeHash.transaction_hash)
+            let res;
 
-            if (res.status === 'ACCEPTED_ON_L2'){
+            try {
+                res = await this.provider.waitForTransaction(executeHash.transaction_hash, { retryInterval: 1500 });
+            } catch (error) {
+                res = false
+            }
 
-                this.logger.info(`${this.moduleString} - Transaction success, but nonce still low`)
-                while(true){
-
-                    let currentNonce = await this.account.getNonce()
-                    await new Promise(resolve => setTimeout(resolve, 30 * 1000));
-
-                    if (currentNonce > nonceCash){
-                        this.logger.info(`${this.moduleString} - The transaction is fully confirmed in the blockchain`)
-                        return
-                    }
-                    else {
-                        this.logger.info(`${this.moduleString} - Nonce is still low, waiting for 30sec...`)
-                        await new Promise(resolve => setTimeout(resolve, 15 * 1000));
+            if (res.status === 'ACCEPTED_ON_L2') {
+                this.logger.info(`${ this.moduleString } - Transaction success, but nonce still low | Nonce ${ nonceCash }`);
+                let currentNonce = await this.account.getNonce()
+                let checker = 0;
+                while (checker < 15) {
+                    await new Promise(resolve => setTimeout(resolve, 10 * 1000));
+                    if (currentNonce > nonceCash) {
+                        this.logger.info(`\x1b[32m${this.moduleString} - The transaction is fully confirmed in the blockchain | Nonce ${currentNonce}\x1b[0m`);
+                        return;
+                    } else {
+                        this.logger.info(`${this.moduleString} - Nonce is still low, waiting for 10sec... | Nonce ${currentNonce}` )
+                        currentNonce = await this.account.getNonce()
+                        checker = checker + 1
                     }
                 }
-
+            } else if (!res) {
+                this.logger.info(`\x1b[31m${ this.moduleString } - Transaction rejected.\x1b[0m`);
+                await new Promise(resolve => setTimeout(resolve, 5 * 1000));
             }
-            else {
-                this.logger.info('error')
-            }
-        } catch (error){
-            this.logger.info(`Error Starknet TX: ${error}`);
+        } catch (e) {
+            this.logger.info(e)
         }
-
     }
 }
