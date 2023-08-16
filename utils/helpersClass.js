8cd828de164e1f774291af0bd1657fe4b34e51cf
alxnode <sasha-panasenya@ya.ru>
2023-08-16 14:20:21 +0300

    Update helpersClass.js
    Fix addLp selection

--- utils/helpersClass.js
+++ utils/helpersClass.js
@@ -86,13 +86,14 @@ export default class helpersFunctions {
 
     getPoolPair(pools){
         try{
-            const randomIndex = Math.floor(Math.random() * pools.length);
-            const selectedPair = pools[randomIndex];
-         
-            const [src, dst] = selectedPair.split('/');
-         
-            pools.splice(randomIndex, 1); 
-            return {src,dst}
+           const poolsCopy = pools.slice(); // Create a copy of the pools array
+        
+        const randomIndex = Math.floor(Math.random() * poolsCopy.length);
+        const selectedPair = poolsCopy[randomIndex];
+
+        const [src, dst] = selectedPair.split('/');
+
+        return { src, dst };
        }catch (error) {
             console.log('ошибка')
             console.log(error)
