766a6a4f0b7ec7421782f4eb84287f910da20e18
fraggdiller <78270254+fraggdiller@users.noreply.github.com>
2023-09-19 21:04:46 +0300

    Update config.js

--- setting/config.js
+++ setting/config.js
@@ -41,7 +41,7 @@ export class OKX {
 
   static withdrawalFromOkxToWallet = false
   static amountToWithdrawal = [0.007, 0.007]
-  static withdrawalToNetwork = 'Arbitrum One' // 'StarkNet'
+  static withdrawalToNetwork = 'Arbitrum One' // 'Starknet'
 
   static withdrawalFromWalletToOkx = false
   static amountToSaveOnWallet = [0.0008, 0.0008]
